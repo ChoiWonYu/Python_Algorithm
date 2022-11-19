08a22bf397e6f422520b09be24b2b10d6c718704
ChoiWonYu <phillip07290@naver.com>
2022-11-19 20:30:08 +0900

    heap,linkedList,stack

---
+++ Stack.py
@@ -0,0 +1,22 @@
+import LinkedListBasic as L
+
+class LinkedStack:
+    def __init__(self):
+        self.__list=L.LinkedListBasic()
+    def push(self,newItem):
+        self.__list.insert(0,newItem)
+    def pop(self):
+        return self.__list.pop(0)
+    def top(self):
+        return self.__list.get(0)
+    def isEmpty(self):
+        return self.__list.isEmpty()
+    def popAll(self):
+        self.__list.clear()
+    def printStack(self):
+        print('Stack from Top:')
+        for i in range(self.__list.size()):
+            print(self.__list.get(i))
+            print()
+
+
