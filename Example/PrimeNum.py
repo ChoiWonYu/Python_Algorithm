27d302da513ef663ca6dc0e5df563bbdef1fd17d
ChoiWonYu <phillip07290@naver.com>
2022-12-09 19:31:20 +0900

    primeNum better code

--- Example/PrimeNum.py
+++ Example/PrimeNum.py
@@ -2,7 +2,9 @@ import math
 s,l=input().split()
 s=int(s)
 l=int(l)
+#s,l=map(int,input().split())
 arr=[True for i in range(l+1)]
+#array[1]=0
 for i in range(2,int(math.sqrt(l))+1):
     j=2
     if arr[i]==True:
@@ -12,6 +14,7 @@ for i in range(2,int(math.sqrt(l))+1):
 
 for i in range(s,l+1):
     if arr[i]==True:
+    #if arr[i]:
         print(i)
 
 
