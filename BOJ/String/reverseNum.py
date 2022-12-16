445dec13fdbc6d276fa66d2f804d0297a53864f1
ChoiWonYu <phillip07290@naver.com>
2022-12-16 16:37:46 +0900

    reverseNum better code

--- BOJ/String/reverseNum.py
+++ BOJ/String/reverseNum.py
@@ -1,5 +1,8 @@
+"""
 n1,n2=input().split()
 f=int(''.join(list(reversed(list(map(str,n1))))))
 s=int(''.join(list(reversed(list(map(str,n2))))))
 if f>s:print(f)
 else: print(s)
+"""
+print(max(list(map(int,input()[::-1].split(' ')))))
