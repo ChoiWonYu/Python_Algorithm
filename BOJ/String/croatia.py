14ef12cdea52896cd7120c003ecc4efde8f757f6
ChoiWonYu <phillip07290@naver.com>
2022-12-16 17:09:55 +0900

    croatia better code

--- BOJ/String/croatia.py
+++ BOJ/String/croatia.py
@@ -1,6 +1,11 @@
 arr=['c=','c-','dz=','d-','lj','nj','s=','z=']
-s=input().strip()
+s=input()
+"""
 sum=0
 for i in arr:
     sum+=s.count(i)
 print(len(s)-sum)
+"""
+for i in arr:
+    s=s.replace(i,'*')
+print(len(s))
