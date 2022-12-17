2d5a12c71a59e8cc2ea80d16e1aae85fdd04c365
ChoiWonYu <phillip07290@naver.com>
2022-12-17 10:05:37 +0900

    slicing comment

--- BOJ/String/reverseNum.py
+++ BOJ/String/reverseNum.py
@@ -6,3 +6,11 @@ if f>s:print(f)
 else: print(s)
 """
 print(max(list(map(int,input()[::-1].split(' ')))))
+
+"""
+list slicing
+a[start:end:step]
+start : slicing을 시작할 인덱스
+end : slicing을 끝낼 인덱스 (끝은 포함하지 않음)
+step ?: 몇개씩 끊어서 가져올지
+"""
