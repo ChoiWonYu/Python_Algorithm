8003a576c3be289e21e66135b013bd5cd181f6be
ChoiWonYu <phillip07290@naver.com>
2022-12-22 01:31:38 +0900

    locationCompress better code

--- BOJ/sort/locationCompress.py
+++ BOJ/sort/locationCompress.py
@@ -2,21 +2,7 @@ import sys
 input=sys.stdin.readline
 n=int(input())
 arr=list(map(int,input().split()))
-length=len(set(arr))
-count=0
-m=0
-while count!=length:
-    if count==0:
-        m=min(arr)
-        for i in range(n):
-            arr[i]-=m
-    else:
-        m=max(arr)
-        for i in range(n):
-            if arr[i]>count-1 and arr[i]<m:
-                m=arr[i]
-        for i in range(n):
-            if arr[i]>count:
-                arr[i]-=m-count
-    count+=1
-print(arr)
+sArr=list(sorted(set(arr)))
+dict={sArr[i]:i for i in range(len(sArr))}
+for i in arr:
+    print(dict[i], end=' ')
