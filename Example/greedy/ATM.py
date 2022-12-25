d2c0b31bdc0a2ff6032b30e91ee2c6725cc5dc2d
ChoiWonYu <phillip07290@naver.com>
2022-12-25 17:07:59 +0900

    ATM better code

--- Example/greedy/ATM.py
+++ Example/greedy/ATM.py
@@ -3,8 +3,6 @@ input=sys.stdin.readline
 N=int(input())
 l=sorted(list(map(int, input().split())))
 s=0
-for idx,i in enumerate(l):
-    s+=i
-    l[idx]=s
-print(sum(l))
-
+for i in range(N):
+    s+=l[i]*(N-i)
+print(s)
