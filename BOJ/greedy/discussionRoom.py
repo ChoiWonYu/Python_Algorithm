8fddf72bc0c5d4e72f61d4d4614ddd3f346924f7
ChoiWonYu <phillip07290@naver.com>
2022-12-25 16:53:39 +0900

    discussionRoom better code

--- BOJ/greedy/discussionRoom.py
+++ BOJ/greedy/discussionRoom.py
@@ -1,20 +1,35 @@
 import sys
 input=sys.stdin.readline
 N=int(input())
-t=[tuple(map(int,input().split())) for _ in range(N)]
-t.sort()
-using=[]
-for x,y in t:
-    l=len(using)
-    if l==0:
-        using.append((x,y))
+d={}
+for _ in range(N):
+    x,y=map(int, input().split())
+    if y in d.keys():
+        d[y].append(x)
     else:
-        if using[-1][1]>y:
-            while using and using[-1][1]>y:
-                using.pop()
-            using.append((x,y))
-        elif using[-1][1]<=x:
-            using.append((x,y))
-print(len(using))
+        d[y]=[x]
+for _ in d.keys():
+    d[_].sort()
+last=0;cnt=0
+for i in sorted(d.keys()):
+    for j in d[i]:
+        if j>=last:
+            cnt+=1
+            last=i
+# t=[tuple(map(int,input().split())) for _ in range(N)]
+# t.sort()
+# using=[]
+# for x,y in t:
+#     l=len(using)
+#     if l==0:
+#         using.append((x,y))
+#     else:
+#         if using[-1][1]>y:
+#             while using and using[-1][1]>y:
+#                 using.pop()
+#             using.append((x,y))
+#         elif using[-1][1]<=x:
+#             using.append((x,y))
+# print(len(using))
 
 
