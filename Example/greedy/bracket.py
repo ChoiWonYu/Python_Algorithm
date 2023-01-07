2b45dfb292a263fde8616ed81e278841bcabb813
ChoiWonYu <phillip07290@naver.com>
2023-01-07 17:21:02 +0900

    bracket better code

--- Example/greedy/bracket.py
+++ Example/greedy/bracket.py
@@ -2,10 +2,7 @@ import sys
 input=sys.stdin.readline
 str=input().strip()
 arr=str.split('-')
-for i in range(len(arr)):
-    arr[i]=list(map(int,arr[i].split('+')))
-    arr[i]=sum(arr[i])
-result=arr[0]
-for i in range(1,len(arr)):
-    result-=arr[i]
-print(result)
+result=[]
+for i in arr:
+    result.append(sum(map(int,i.split('+'))))
+print(result[0]-sum(result[1:]))
