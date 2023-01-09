4e9fc7a5986593de52a60ca293026bbf8c7bdc84
ChoiWonYu <phillip07290@naver.com>
2023-01-09 12:42:34 +0900

    NumCard better code

--- BOJ/SetMap/NumCard2.py
+++ BOJ/SetMap/NumCard2.py
@@ -2,9 +2,10 @@ import sys
 input=sys.stdin.readline
 N=int(input())
 card={}
-l=list(map(int,input().split()))
+l=input().split()
 for i in l:
     if i in card:card[i]+=1
     else: card[i]=1
 M=int(input())
-print(' '.join(map(lambda x:str(card[int(x)]) if int(x) in card else '0',input().split())))
+S=input().split()
+print(' '.join(str(card[x]) if x in card else '0' for x in S))
