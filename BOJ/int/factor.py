122940b8e313d9c5af4a627451a9ce6acb7b128c
ChoiWonYu <phillip07290@naver.com>
2023-01-15 09:58:33 +0900

    facotr bettercode

--- BOJ/int/factor.py
+++ BOJ/int/factor.py
@@ -1,5 +1,5 @@
 import sys
 input=sys.stdin.readline
 count=int(input())
-arr=list(map(int,input().split()))
-print(max(arr)*min(arr))
+arr=sorted(list(map(int,input().split())))
+print(arr[0]*arr[-1])
