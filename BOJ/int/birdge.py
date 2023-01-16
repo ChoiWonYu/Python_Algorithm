c05bd39b2466fefe286fdbe4dbc8d1ccdbf791f8
ChoiWonYu <phillip07290@naver.com>
2023-01-16 17:18:41 +0900

    bridge better code

--- BOJ/int/birdge.py
+++ BOJ/int/birdge.py
@@ -4,12 +4,9 @@ input=sys.stdin.readline
 c=int(input())
 for i in range(c):
     n,m=map(int,input().split())
-    if m - n == 0:print(1)
-    else:
-        a,b=0,0;result=1
-        for j in range(1,m+1):
-            result*=j
-            if j==n:a=result
-            if j==(m-n):b=result
-
-        print(result//(a*b))
+    a,b=1,1
+    for i in range(m,m-n,-1):
+        a*=i
+    for j in range(n,1,-1):
+        b*=j
+    print(a//b)
