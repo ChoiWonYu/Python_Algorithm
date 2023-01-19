7cacf6272ae262ce1d4fbe1272b7cfa8712018c6
ChoiWonYu <phillip07290@naver.com>
2023-01-19 20:47:32 +0900

    checkpoint

--- BOJ/int/checkpoint.py
+++ BOJ/int/checkpoint.py
@@ -1,3 +1,22 @@
 import sys
-input=sys.stdin.readlines
+input=sys.stdin.readline
 count=int(input())
+l=sorted(int(input()) for _ in range(count))
+newL=[]
+for i in range(len(l)-1):
+    newL.append(l[i+1]-l[i])
+def gcd(a,b):
+    while b:
+        a,b=b,a%b
+    return a
+
+GCD = newL[0]
+for idx in range(1, len(newL)):
+    GCD = gcd(GCD, newL[idx])
+result = set()
+for i in range(2, int(GCD**0.5) + 1):
+    if GCD % i == 0:
+        result.add(i)
+        result.add(GCD // i)
+result.add(GCD)
+print(*sorted(list(result)))
