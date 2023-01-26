31262aa022276f424014dace682b669d64bb4658
ChoiWonYu <phillip07290@naver.com>
2023-01-26 11:22:11 +0900

    NM2 bettercode

--- BOJ/backtracking/NM2.py
+++ BOJ/backtracking/NM2.py
@@ -1,8 +1,6 @@
-import sys,itertools
+import sys
+from itertools import combinations
 input=sys.stdin.readline
 N,M=map(int,input().split())
-result=list(itertools.combinations([i for i in range(1,N+1)],M))
-for i in result:
-    for j in i:
-        print(j,end=' ')
-    print()
+for i in combinations([i for i in range(1,N+1)],M):
+    print(*i)
