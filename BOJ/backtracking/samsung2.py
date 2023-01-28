a7356949e00244434de6149809c06971e0af4b1a
ChoiWonYu <phillip07290@naver.com>
2023-01-28 13:02:55 +0900

    samsung2 better code

--- BOJ/backtracking/samsung2.py
+++ BOJ/backtracking/samsung2.py
@@ -1,28 +1,13 @@
 import sys
-from itertools import combinations
-input=sys.stdin.readline
-startLink=[]
-N=int(input())
-for i in range(N):
-    startLink.append(list(map(int,input().split())))
+from itertools import combinations as cb
+N = int(sys.stdin.readline()) // 2
+M = 2*N
+stat = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+newstat = [sum(i) + sum(j) for i, j in zip(stat, zip(*stat))]
+#2차원 배열의 열은 zip(*list)로 얻어낼 수 있다.
+allstat = sum(newstat) // 2
 
-team=[i for i in range(N)]
-teams=list(combinations(team,N//2))
-min=1000000000000
-total=len(teams)
-for i in range(total):
-    if i==total//2:break
-    start=0
-    link=0
-    oth=teams[-(i+1)]
-    startT=teams[i]
-    for j in range(N//2):
-        for m in range(N//2):
-            if m!=j:
-                start+=startLink[startT[j]][startT[m]]
-                link+=startLink[oth[j]][oth[m]]
-    if (start-link)<0:
-        if link-start<min:min=link-start
-    else:
-        if start-link<min:min=start-link
-print(min)
+mins = 65535
+for l in cb(newstat[:-1], N):
+    mins = min(mins, abs(allstat - sum(l)))
+print(mins)
