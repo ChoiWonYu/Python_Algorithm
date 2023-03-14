5166b9c49f866ca5016d275d291f96563a3c9027
ChoiWonYu <phillip07290@naver.com>
2023-03-14 09:06:41 +0900

    feat: printer

--- BOJ/queue/printer.py
+++ BOJ/queue/printer.py
@@ -1,2 +1,23 @@
 import sys
 from collections import deque
+
+input=sys.stdin.readline
+T=int(input())
+for i in range(T):
+    N,M=map(int,input().split())
+    arr=deque(map(int,input().split()))
+    s=0
+    c=1
+    while 1:
+        if max(arr)==arr[0]:
+            if s%N==M:
+                print(c)
+                break
+            else:
+                arr[0]=0
+                arr.rotate(-1)
+                c+=1
+        else:
+            arr.rotate(-1)
+        s+=1
+
